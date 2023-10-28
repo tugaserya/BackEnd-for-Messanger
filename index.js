1d51ebb41352491ff08bac4aba4fe5df2c542632
tugaserya <artempoznakov990@gmail.com>
2023-10-29 00:29:01 +0300

    dsds

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const options = {
 const userRouter = require('./routes/user.routes');
 const chatRouter = require('./routes/chats.routes');
 const messagesRouter = require('./routes/messages.routes');
-const { Model } = require('firebase-admin/lib/machine-learning/machine-learning.js');
+
 const app = express();
 
 app.use(express.json()); // Распарсить JSON response
