06929f4c708cf3804f4df44df791f85cdd515658
tugaserya <artempoznakov990@gmail.com>
2023-11-01 15:56:23 +0300

    path ws err

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
-const wsModule = require('./websockets')
+const wsModule = require('./websokets')
 
 
 const PORT = process.env.PORT || 4200;
