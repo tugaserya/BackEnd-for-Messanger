51bdf12eead279255cf770282b9f148fe8d28ffc
tugaserya <artempoznakov990@gmail.com>
2023-11-10 18:44:13 +0300

    wwww

--- userChecker.js
+++ userChecker.js
@@ -2,10 +2,12 @@ const db = require('./db')
 const bcrypt = require('bcrypt')
 
 module.exports.UserChecker = async (login, password) => {
-    const userData = await db.query('SELECT * FROM users WHERE login = $1', [login]);
+    const userData = await db.query('SELECT * FROM users WHERE login = $1', [login])
+    console.log("work 1");
             if (userData.rows.length > 0) {
                 const validPassword = await bcrypt.compare(password, userData.rows[0].password);
                 if (validPassword) {
+                    console.log("work 2");
                     return true
                 } else {
                     return false
