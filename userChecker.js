da2c7db90adba3155a5c4cd9326a1b3c141ee0e6
tugaserya <artempoznakov990@gmail.com>
2023-11-10 18:56:41 +0300

    wwww

--- userChecker.js
+++ userChecker.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt')
 
 module.exports.UserChecker = async (login, password) => {
     const userData = await db.query('SELECT * FROM users WHERE login = $1', [login])
-    console.log("work 1");
+    console.log("login " + login + "\npassword " + password);
             if (userData.rows.length > 0) {
                 console.log("work 2");
                 const validPassword = await bcrypt.compare(password, userData.rows[0].password)
