c61ae137f9758a7c8f7c884f462174fc43d96972
tugaserya <artempoznakov990@gmail.com>
2023-11-10 18:05:20 +0300

    dsdsddddd

--- routes/chats.routes.js
+++ routes/chats.routes.js
@@ -4,6 +4,6 @@ const  ChatsController = require('../controller/chats.controller')
 //пути URL для вызова функций по запросу
 router.post('/chat_creating', ChatsController.createChat)
 router.post('/get_chats', ChatsController.getChat)
-router.post('/archive_chat', ChatsController.archiveChat)
+//router.post('/archive_chat', ChatsController.archiveChat)
 
 module.exports = router
--- routes/messages.routes.js
+++ routes/messages.routes.js
@@ -4,7 +4,7 @@ const messageController = require('../controller/message.controller')
 
 //пути URL для вызова функций по запросу
 router.post('/get_past_messages', messageController.getPastMessages)
-router.post('/update_message', messageController.updateMessage)
-router.post('/archive_message', messageController.archiveMessage)
+// router.post('/update_message', messageController.updateMessage)
+// router.post('/archive_message', messageController.archiveMessage)
 
 module.exports = router
