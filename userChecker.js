7e0e05520f48ad470e56737fcca09f2da56cd33e
tugaserya <artempoznakov990@gmail.com>
2023-11-10 19:13:25 +0300

    add new UserCheck

--- userChecker.js
+++ userChecker.js
@@ -3,13 +3,9 @@ const bcrypt = require('bcrypt')
 
 module.exports.UserChecker = async (login, password) => {
     const userData = await db.query('SELECT * FROM users WHERE login = $1', [login])
-    console.log("login " + login + "\npassword " + password);
             if (userData.rows.length > 0) {
-                console.log("work 2");
                 const validPassword = await bcrypt.compare(password, userData.rows[0].password)
-                console.log("work 3 " + validPassword)
                 if (validPassword) {
-                    console.log("work 4");
                     return true
                 } else {
                     return false
