f057ce2e6e4b83277de263153f464527fca2b609
tugaserya <artempoznakov990@gmail.com>
2023-11-14 22:22:19 +0300

    dsdsd

--- websokets.js
+++ websokets.js
@@ -119,7 +119,7 @@ module.exports.initWebSocket = (server) => {
                                     if (clients.has(String(message.rows[0].recipient_id)) && clients.has(String(message.rows[0].sender_id))) {
                                         const recipient_ws = clients.get(String(message.rows[0].recipient_id));
                                         const sender_ws = clients.get(String(message.rows[0].sender_id));
-                                        recipient_ws.send(JSON.stringify({ deleting_message_id, type: "delete_message" }));
+                                        recipient_ws.send(JSON.stringify({ message_id: deleting_message_id, type: "delete_message" }));
                                         sender_ws.send(JSON.stringify({ message_id: deleting_message_id, type: "delete_message" }))
                                     }
                                 }
