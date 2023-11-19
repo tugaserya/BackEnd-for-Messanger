34c6e11471b03906986284e53c20a7095cd49eb7
tugaserya <artempoznakov990@gmail.com>
2023-11-19 21:27:47 +0300

    dsd

--- controller/file_uploads.controller.js
+++ controller/file_uploads.controller.js
@@ -1,52 +1,46 @@
-const db = require('../db')
-const {UserChecker} = require("../userChecker");
-const multer  = require('multer');
-
-
-
 class FileUploadsController {
 
     async AvatarsUpload (req, res){
         try{
-            const params = new URL(req.url).searchParams
-            const login = params.get("login")
-            const password = params.get("password");
-            if(UserChecker(login, password)){
-                const user = await db.query(`SELECT * FROM users WHERE login = $1;`,[login])
-                let storage = multer.diskStorage({
-                    destination: function (req, file, cb) {
-                        cb(null, '../uploads/avatars')
-                    },
-                    filename: function (req, file, cb) {
-                        let newFileName = `${user.rows[0].id}_${user.rows[0].login}` + path.extname(file.originalname);
-                        cb(null, newFileName);
+            let storage = multer.diskStorage({
+                destination: function (req, file, cb) {
+                    cb(null, '../uploads/avatars')
+                },
+                filename: function (req, file, cb) {
+                    let newFileName = `${req.body.id}_${req.body.login}` + path.extname(file.originalname);
+                    cb(null, newFileName);
+                }
+            });
+            
+            let upload = multer({
+                storage: storage,
+                limits: {
+                    fileSize: 10485760
+                },
+                fileFilter: function (req, file, cb) {
+                    if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png') {
+                        return cb(new Error('Неверный тип файла'));
                     }
-                });
-                
-                let upload = multer({
-                    storage: storage,
-                    limits: {
-                        fileSize: 10485760
-                    },
-                    fileFilter: function (req, file, cb) {
-                        if (file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png') {
-                            return cb(new Error('Неверный тип файла'));
-                        }
-                        cb(null, true);
-                    }
-                }).single('file');
-                
-                
-                upload(req, res, function (err) {
-                    if (err) {
-                        console.log('not work');
-                        res.status(500).json({message: "Ошибка загрузкифайла"})
-                    } else {
+                    cb(null, true);
+                }
+            }).single('file');
+            
+            
+            upload(req, res, async function (err) {
+                if (err) {
+                    console.log('not work');
+                    res.status(500).json({message: "Ошибка загрузки файла"})
+                } else {
+                    const { login, password } = req.body
+                    if(UserChecker(login, password)){
                         console.log('work');
                         res.status(200).json({message: "Файл загружен"})
+                    } else { 
+                        console.log('work'); 
+                        res.status(418).json({message: "ВОН АЛКАШ"})
                     }
-                });
-            } else { console.log('work'); res.status(418).json({message: "ВОН АЛКАШ"})}
+                }
+            });
         } catch(err){
                 console.error(err);
         }
