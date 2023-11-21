1ac71066f43c59915d72ecc660479939268f7f34
tugaserya <artempoznakov990@gmail.com>
2023-11-21 20:50:40 +0300

    dsdsdd

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
-const path = require('path')
 const wsModule = require('./websokets/websokets')
-
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 4200;
 const testPORT = 2228;
 const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/domenforallnames.space/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/domenforallnames.space/fullchain.pem')
+    key: fs.readFileSync('/etc/letsencrypt/live/domenforallnames.space/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/domenforallnames.space/fullchain.pem')
 };
 
 
-
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 const userRouter = require('./routes/user.routes');
 const chatRouter = require('./routes/chats.routes');
@@ -32,7 +32,7 @@ app.use('/', file_uploadsRouter)
 const server = https.createServer(options, app);
 
 server.listen(PORT, () => {
-  console.log('Сервер слушает на порту', PORT);
+    console.log('Сервер слушает на порту', PORT);
 });
 
 wsModule.initWebSocket(server);
