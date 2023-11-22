a3329d0b6172114a2bdf959e8548a43315ede1db
tugaserya <artempoznakov990@gmail.com>
2023-11-22 17:04:53 +0300

    dsdsd

--- controller/file_uploads.controller.js
+++ controller/file_uploads.controller.js
@@ -1,5 +1,8 @@
 const db = require('../db')
 const {UserChecker} = require("../userChecker");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 
 
 class FileUploadsController {
@@ -46,6 +49,76 @@ class FileUploadsController {
             }
         } catch(err){
             console.error(err);        }
+    }
+
+    async FileUpload(req, res){
+        try{
+            let fileoriginalname;
+            let newfilename
+            let storage = multer.diskStorage({
+                destination(req, file, cb) {
+                    let uploadPath;
+                    switch (req.body.filetype) {
+                        case 'image':
+                            uploadPath = path.join(__dirname, '../../uploads/image/');
+                        case 'media':
+                            uploadPath = path.join(__dirname, '../../uploads/media/');
+                        case 'docs':
+                            uploadPath = path.join(__dirname, '../../uploads/docs/');
+                        case 'other':
+                            uploadPath = path.join(__dirname, '../../uploads/other/');
+                    }
+                    cb(null, uploadPath);
+                },
+                async filename (req, file, cb) {
+                    if(!(await UserChecker(req.body.user_id, req.body.login, req.body.password))){
+                        res.status(500).json({message: "Неверный пользователь"})
+                        return
+                    }
+                    fileoriginalname = file.originalname
+                    newfilename = `${Date.now()}_${file.originalname}`
+                    await db.query(`UPDATE messages SET file = $1, file_type = $2 WHERE id = 3;`,[newfilename, req.body.filetype, req.body.message_id])
+                    cb(null, newfilename);
+                }
+            });
+
+            let upload = multer({
+                storage,
+                limits: {
+                    fileSize: 90971520
+                },
+                fileFilter: function (req, file, cb) {cb(null, true);}
+            }).single('file');
+
+            upload(req, res, async function (err) {
+                if (err) {
+                    console.log(err);
+                    res.status(500).json({message: "Ошибка загрузки файла"})
+                } else {
+                        res.status(200).json({filename: fileoriginalname, newfilename: newfilename})
+                }
+            });
+        } catch (err){
+            console.error(err)
+        }
+    }
+
+    async FileDownload(req, res){
+        try {
+            const {message_id, user_id, login, password} = req.body
+            if(await UserChecker(user_id, login, password)){
+                const message = await db.query(`SELECT * FROM messages WHERE id = $1;`, [message_id])
+                if(message.rows[0].sender_id === user_id || message.rows[0].recipient_id === user_id) {
+                    res.sendFile(path.join(__dirname, '../../uploads/' + message.rows[0].file_type + '/' + message.rows[0].file))
+                } else{
+                    res.status(418).json({message: "id пользователя не найден ВОН АЛКАШ"})
+                }
+            }else {
+                res.status(418).json({message: "ВОН АЛКАШ"})
+            }
+        }catch (err){
+            console.error(err)
+        }
     }
 }
 
