c2970a572f920f4ba40f5a2065bf25806c7b77d7
tugaserya <artempoznakov990@gmail.com>
2023-11-22 17:08:55 +0300

    dsdsd

--- routes/file_uploads.router.js
+++ routes/file_uploads.router.js
@@ -6,6 +6,6 @@ router.post('/avatar_upload', FileUploadsController.AvatarsUpload)
 router.post('/avatar_download', FileUploadsController.AvatarDownload)
 router.post('/avatar_delete', FileUploadsController.DeleteAvatar)
 router.post('/file_upload', FileUploadsController.FileUpload)
-router.post('file_download', FileUploadsController.FileDownload)
+router.post('/file_download', FileUploadsController.FileDownload)
 
 module.exports = router
